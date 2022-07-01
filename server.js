a82ecb03698208dfdedbc1d32824dc71981064e3
CHANDRAMANI KUMAR PATEL <so7085490667@gmail.com>
2022-07-01 18:06:59 +0530

    add cart page, auth

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
 
 //Assets
 app.use(express.static('public'))
@@ -16,6 +13,18 @@ app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/cart',(req, res)=>{
+    res.render('customers/cart')
+})
+app.get('/login',(req, res)=>{
+    res.render('auth/login')
+})
+app.get('/register',(req, res)=>{
+    res.render('auth/register')
+})
 app.listen(PORT, ()=> {
     //black tick for including variable inside this
     console.log(`Listening on port ${PORT}`)

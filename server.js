f3a3750f9460d01d9cb7d7e72f9b344041ad2ae4
CHANDRAMANI KUMAR PATEL <so7085490667@gmail.com>
2022-07-02 18:59:01 +0530

    build auth system

--- server.js
+++ server.js
@@ -1,69 +1,85 @@
-const express = require('express')
+// Creating express server
+
+// to give access to env file our applications 
 require('dotenv').config()
-const app = express()
-const ejs = require('ejs')
-const path = require('path')
-const expressLayout = require('express-ejs-layouts')
-const PORT = process.env.PORT || 3000
-const mongoose = require('mongoose')
+// importing express js in a variable express
+
+const express = require('express')
+// At this point funciton has been imported in express
+// setting up session
 const session = require('express-session')
-const flash =require('express-flash')
-const MongoDbStore = require('connect-mongo')
+// importing ejs
+const ejs = require('ejs')
+
+const path = require('path') // inbuilt module of node js
+
+// importing express layout
+const expressLayout = require('express-ejs-layouts')
 const Noty = require('noty')
-//database connection
-const url = 'mongodb://localhost/pizza';
+const app = express() // this will return the main object of express and stores in app variable
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true});
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('Database connected...');
-}).on('error', (err)=> {
-    console.log('Connection failed...')
-});
+const PORT = process.env.PORT || 3300
+// the above code is checking if there any prdefined suitable port
+// available in the environment or not if available then select that
+// other wise set it to 3000
+const mongoose = require('mongoose')
 
-//for session storage
-//Session Store
-const clientP = mongoose.connect(url,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  ).then(m => m.connection.getClient())
+const flash = require('express-flash')
+app.use(flash())
+const MongoDbStore = require('connect-mongo')
+// Data Base Connection
+const passport = require('passport')
+const url = "mongodb://localhost/pizza";
 
-//Session Config, session also acts as a middleware
-//session are valid till the cookies are valid
+mongoose.connect(url,
+    err =>{
+        if(err) throw err;
+        console.log('Connected to Database')
+    });
+
+// Assets
+app.use(express.static('public')) // express by default does not serve static files so this function is used 
+// to serve static files like css, scripts, images etc...
+
+// enabling sever to recieve json request
+app.use(express.json())
+app.use(express.urlencoded({extended: false})) // enabling sever to recieve url encoded data
+// session configuration
 app.use(session({
-    //to encrypt the cookies
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
+    secret : process.env.COOKIE_SECRET,
+    resave : false,
     store: MongoDbStore.create({
-        clientPromise : clientP,
-        collection: 'sessions'
-    }),
-    saveUninitialized: false,
-    // cookies live // down one is 24 hours
-    cookie: {maxAge: 1000 * 60 * 60 * 24}
+        mongoUrl : url
+    }),  // this code helps in storing sessions in database
+    saveUninitialized : false,
+    cookie : {maxAge: 1000 * 60 * 60 * 24} // age of cookies in ms here it is equal to 24 hours
 }))
 
-app.use(flash())
 
+// Passport Config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
 
-//Assets
-app.use(express.static('public'))
-app.use(express.json())
-
-//Global midddleware
-app.use((req, res, next)=>{
+// by default the session's key is not available in frontend so we have to make it global using middleware
+app.use((req,res,next) =>{
     res.locals.session = req.session
-    next()
+    res.locals.user = req.user
+    next() // this is mandatory to use otherwise the req will not pass ahead
 })
 
-//set Template engine
-app.use(expressLayout)
+// set template engine
+app.use(expressLayout) 
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-require('./routes/web')(app)
+require('./routes/web')(app) // In javascript every function is passed by refrence
 
-
-app.listen(PORT, ()=> {
-    //black tick for including variable inside this
-    console.log(`Listening on port ${PORT}`)
+app.listen(PORT, () => { 
+// what basically listen does is that it prepares the specified 
+// port to listen request and printing response written later in 
+// the funciton in termial
+    console.log(`Listening on port ${PORT}`) // backticks (`) is used to insert port as a variable
 })
+// now the server is created
